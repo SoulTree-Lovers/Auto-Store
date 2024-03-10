1db39a52d534e557dfb1f80600bf7dd5041651da
kangseungmin <rkdtmdals999@naver.com>
2024-03-10 21:31:08 +0900

    Create 'Auto Buyer' Project

---
+++ src/main/java/com/example/autobuyer/AutoBuyerApplication.java
@@ -0,0 +1,13 @@
+package com.example.autobuyer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoBuyerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutoBuyerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/autobuyer/AutoBuyerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.autobuyer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoBuyerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
