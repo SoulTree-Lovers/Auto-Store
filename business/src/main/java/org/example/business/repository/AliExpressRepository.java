ed6b0f88f169b95ffe7cf188e3a2d7f7e963ec50
kangseungmin <rkdtmdals999@naver.com>
2024-03-18 23:07:58 +0900

    [feat] Product -> ProductEntity

--- business/src/main/java/org/example/business/repository/AliExpressRepository.java
+++ business/src/main/java/org/example/business/repository/AliExpressRepository.java
@@ -1,9 +1,9 @@
 package org.example.business.repository;
 
-import org.example.business.repository.entity.Product;
+import org.example.business.repository.entity.ProductEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface AliExpressRepository extends JpaRepository<Product, Long> {
+public interface AliExpressRepository extends JpaRepository<ProductEntity, Long> {
 
     /**
      * TODO: 알리 익스프레스에서 가져온 상품 정보를 DB에 저장하는 로직 추가하기
