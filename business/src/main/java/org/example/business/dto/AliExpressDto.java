74c3154df260976c19009fe5a7eb5099961e3f7f
kangseungmin <rkdtmdals999@naver.com>
2024-03-18 23:07:15 +0900

    [feat] Data 어노테이션 추가

--- business/src/main/java/org/example/business/dto/AliExpressDto.java
+++ business/src/main/java/org/example/business/dto/AliExpressDto.java
@@ -1,4 +1,18 @@
 package org.example.business.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class AliExpressDto {
+
+    /**
+     * TODO: 알리 익스프레스에서 가져온 상품 정보에 대한 속성 추가하기.
+     */
+
 }
