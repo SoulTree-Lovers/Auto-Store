da1051c243c3c4588cff299c171343bd07e97c2f
kangseungmin <rkdtmdals999@naver.com>
2024-03-18 23:07:37 +0900

    [feat] 이름 변경 및 Data 어노테이션 추가

--- business/src/main/java/org/example/business/repository/entity/ProductEntity.java
+++ business/src/main/java/org/example/business/repository/entity/ProductEntity.java
@@ -1,10 +1,16 @@
 package org.example.business.repository.entity;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
 
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 @Table(name = "product")
-public class Product {
+public class ProductEntity {
     /**
      * DB에 넣을 상품들의 정보가 담긴 테이블
      */
