2bf224ece12bb35d22c085ce05ee5bb528d7b34c
kangseungmin <rkdtmdals999@naver.com>
2024-03-18 23:07:08 +0900

    [feat] Data 어노테이션 추가

--- business/src/main/java/org/example/business/dto/NaverDto.java
+++ business/src/main/java/org/example/business/dto/NaverDto.java
@@ -1,4 +1,18 @@
 package org.example.business.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class NaverDto {
+
+    /**
+     * TODO: Naver에 상품을 등록하기 위한 속성 추가하기.
+     */
+
 }
