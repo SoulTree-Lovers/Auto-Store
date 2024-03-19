28f5c7216d8d3148a8fe1172099e4f1dff8b3b1f
kangseungmin <rkdtmdals999@naver.com>
2024-03-19 17:52:46 +0900

    [refactor] NaverDto -> NaverRequest로 변경 (확실한 표기를 위함)

--- business/src/main/java/org/example/business/model/NaverRequest.java
+++ business/src/main/java/org/example/business/model/NaverRequest.java
@@ -1,4 +1,4 @@
-package org.example.business.dto;
+package org.example.business.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -9,7 +9,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
-public class NaverDto {
+public class NaverRequest {
 
     /**
      * TODO: Naver에 상품을 등록하기 위한 속성 추가하기.
