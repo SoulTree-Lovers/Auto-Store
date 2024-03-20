0aec75f91d59f5a777ac4389e2a3eb79f3bdbc05
kangseungmin <rkdtmdals999@naver.com>
2024-03-20 16:54:56 +0900

    [refactor] UserController 프로젝트 위치 변경

--- api/src/main/java/org/example/api/domain/user/controller/UserController.java
+++ api/src/main/java/org/example/api/domain/user/controller/UserController.java
@@ -1,4 +1,4 @@
-package org.example.api.controller;
+package org.example.api.domain.user.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
