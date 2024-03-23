02663f1710076a131c2f697bc407d2e0650ff112
kangseungmin <rkdtmdals999@naver.com>
2024-03-23 22:50:44 +0900

    [feat] StoreAdminRequest 생성

---
+++ api/src/main/java/org/example/api/domain/storeadmin/controller/model/StoreAdminRequest.java
@@ -0,0 +1,19 @@
+package org.example.api.domain.storeadmin.controller.model;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class StoreAdminRequest {
+
+    /**
+     * TODO: StoreAdmin 요청 DTO
+     */
+
+}
