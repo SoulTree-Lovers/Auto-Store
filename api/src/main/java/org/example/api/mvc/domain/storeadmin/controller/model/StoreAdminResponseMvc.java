a2e3bc26b1c3cc5ffca353d7357347d7d43dc0fa
kangseungmin <rkdtmdals999@naver.com>
2024-03-30 19:20:26 +0900

    [feat] AllArgsConstructor Annotation 임시 주석 (속성 추가 전이라 에러 발생)

--- api/src/main/java/org/example/api/mvc/domain/storeadmin/controller/model/StoreAdminResponseMvc.java
+++ api/src/main/java/org/example/api/mvc/domain/storeadmin/controller/model/StoreAdminResponseMvc.java
@@ -1,13 +1,12 @@
 package org.example.api.mvc.domain.storeadmin.controller.model;
 
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
-@AllArgsConstructor
+//@AllArgsConstructor
 @Builder
 public class StoreAdminResponseMvc {
 
