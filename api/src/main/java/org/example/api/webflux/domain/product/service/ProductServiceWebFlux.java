77dffad15fb234c29005a8e9f344dd6b7b094a7a
kangseungmin <rkdtmdals999@naver.com>
2024-03-30 19:39:49 +0900

    [feat] CRUD Service 로직 구현

--- api/src/main/java/org/example/api/webflux/domain/product/service/ProductServiceWebFlux.java
+++ api/src/main/java/org/example/api/webflux/domain/product/service/ProductServiceWebFlux.java
@@ -1,12 +1,55 @@
 package org.example.api.webflux.domain.product.service;
 
+import lombok.RequiredArgsConstructor;
+import org.example.api.webflux.domain.product.repository.ProductEntityWebFlux;
+import org.example.api.webflux.domain.product.repository.ProductRepositoryWebFlux;
 import org.springframework.stereotype.Service;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 @Service
+@RequiredArgsConstructor
 public class ProductServiceWebFlux {
 
     /**
      * TODO: 상품 CRUD 서비스 로직 구현
      */
+
+    private final ProductRepositoryWebFlux productRepositoryWebFlux;
+
+    // Create
+    public Mono<ProductEntityWebFlux> create(
+        Long adminId,
+        String name,
+        Long price,
+        String category,
+        String thumbnailUrl
+    ) {
+
+        return productRepositoryWebFlux.save(
+            ProductEntityWebFlux.builder()
+                .adminId(adminId)
+                .name(name)
+                .price(price)
+                .category(category)
+                .thumbnailUrl(thumbnailUrl)
+                .build()
+        );
+    }
+
+    // Read
+    public Flux<ProductEntityWebFlux> findAll() {
+        return productRepositoryWebFlux.findAll();
+    }
+
+    // Update
+    public Mono<ProductEntityWebFlux> findById(Long id) {
+        return productRepositoryWebFlux.findById(id);
+    }
+
+    // Delete
+    public Mono<Void> deleteById(Long id) {
+        return productRepositoryWebFlux.deleteById(id);
+    }
 
 }
