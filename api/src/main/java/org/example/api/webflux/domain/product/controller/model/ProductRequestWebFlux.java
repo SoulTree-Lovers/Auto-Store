31136411a9d0614fd4f5ea3b4c65de118222f9a8
kangseungmin <rkdtmdals999@naver.com>
2024-03-30 19:23:07 +0900

    [feat] Webflux에서 상품 Request DTO 속성 추가

--- api/src/main/java/org/example/api/webflux/domain/product/controller/model/ProductRequestWebFlux.java
+++ api/src/main/java/org/example/api/webflux/domain/product/controller/model/ProductRequestWebFlux.java
@@ -1,16 +1,28 @@
 package org.example.api.webflux.domain.product.controller.model;
 
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDateTime;
+
 @Data
 @NoArgsConstructor
-@AllArgsConstructor
-@Builder
+//@AllArgsConstructor
 public class ProductRequestWebFlux {
 
+    private Long adminId; // 판매자 아이디
+
+    private String name; // 상품 이름
+
+    private Long price; // 상품 가격
+
+    private String category; // 상품 카테고리
+
+    private String thumbnailUrl; // 상품 썸네일 이미지 주소
+
+    private LocalDateTime createdAt;
+
+    private LocalDateTime updatedAt;
 
 }
