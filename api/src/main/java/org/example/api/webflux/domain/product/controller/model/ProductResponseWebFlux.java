6d383ba6d6cc242cd0ab771d22681fd49e16cc3b
kangseungmin <rkdtmdals999@naver.com>
2024-03-30 19:23:19 +0900

    [feat] Webflux에서 상품 Response DTO 속성 추가

--- api/src/main/java/org/example/api/webflux/domain/product/controller/model/ProductResponseWebFlux.java
+++ api/src/main/java/org/example/api/webflux/domain/product/controller/model/ProductResponseWebFlux.java
@@ -4,6 +4,9 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.example.api.webflux.domain.product.repository.ProductEntityWebFlux;
+
+import java.time.LocalDateTime;
 
 @Data
 @NoArgsConstructor
@@ -11,5 +14,29 @@ import lombok.NoArgsConstructor;
 @Builder
 public class ProductResponseWebFlux {
 
+    private Long adminId; // 판매자 아이디
 
+    private String name; // 상품 이름
+
+    private Long price; // 상품 가격
+
+    private String category; // 상품 카테고리
+
+    private String thumbnailUrl; // 상품 썸네일 이미지 주소
+
+    private LocalDateTime createdAt;
+
+    private LocalDateTime updatedAt;
+
+    public static ProductResponseWebFlux of(ProductEntityWebFlux productEntityWebFlux) {
+        return ProductResponseWebFlux.builder()
+            .adminId(productEntityWebFlux.getAdminId())
+            .name(productEntityWebFlux.getName())
+            .price(productEntityWebFlux.getPrice())
+            .category(productEntityWebFlux.getCategory())
+            .thumbnailUrl(productEntityWebFlux.getThumbnailUrl())
+            .createdAt(productEntityWebFlux.getCreatedAt())
+            .updatedAt(productEntityWebFlux.getUpdatedAt())
+            .build();
+    }
 }
