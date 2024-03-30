13273341e8df955394fc225384671e981cac89c3
kangseungmin <rkdtmdals999@naver.com>
2024-03-30 19:39:35 +0900

    [feat] Repository 어노테이션 추가

--- api/src/main/java/org/example/api/webflux/domain/product/repository/ProductRepositoryWebFlux.java
+++ api/src/main/java/org/example/api/webflux/domain/product/repository/ProductRepositoryWebFlux.java
@@ -1,7 +1,11 @@
 package org.example.api.webflux.domain.product.repository;
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Qualifier(value = "product")
+@Repository
 public interface ProductRepositoryWebFlux extends ReactiveCrudRepository<ProductEntityWebFlux, Long> {
 
 
