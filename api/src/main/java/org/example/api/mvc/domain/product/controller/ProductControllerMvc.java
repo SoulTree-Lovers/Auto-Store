a253a66f6b9e1af38e7ae972c2c34ceb388e7990
kangseungmin <rkdtmdals999@naver.com>
2024-03-31 16:13:36 +0900

    [feat] Mvc 기반 Product Controller CRUD 구현

--- api/src/main/java/org/example/api/mvc/domain/product/controller/ProductControllerMvc.java
+++ api/src/main/java/org/example/api/mvc/domain/product/controller/ProductControllerMvc.java
@@ -1,14 +1,68 @@
 package org.example.api.mvc.domain.product.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import lombok.RequiredArgsConstructor;
+import org.example.api.mvc.domain.product.controller.model.ProductRequestMvc;
+import org.example.api.mvc.domain.product.controller.model.ProductResponseMvc;
+import org.example.api.mvc.domain.product.converter.ProductConverter;
+import org.example.api.mvc.domain.product.service.ProductServiceMvc;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
-@RequestMapping("/product")
+@RequiredArgsConstructor
+@RequestMapping("/mvc/product")
 public class ProductControllerMvc {
 
     /**
      * TODO: 상품 CRUD 요청 구현
      */
 
+    private final ProductServiceMvc productServiceMvc;
+    private final ProductConverter productConverter;
+
+    // Create
+    @PostMapping("/create")
+    public ProductResponseMvc createProduct(
+        @RequestBody ProductRequestMvc request
+        ) {
+
+        var productEntity = productServiceMvc.create(
+            request.getAdminId(),
+            request.getName(),
+            request.getPrice(),
+            request.getCategory(),
+            request.getThumbnailUrl()
+        );
+
+        return productConverter.toResponse(productEntity);
+
+    }
+
+    // Read
+    @GetMapping("/find-all")
+    public List<ProductResponseMvc> findAllProduct() {
+        return productServiceMvc.findAll().stream()
+            .map(productConverter::toResponse)
+            .toList();
+    }
+
+    @GetMapping("/find/{id}")
+    public ProductResponseMvc findProductById(
+        @PathVariable Long id
+    ) {
+        var productEntity = productServiceMvc.findById(id);
+        return productConverter.toResponse(productEntity);
+    }
+
+    // Update
+
+
+    // Delete
+    @DeleteMapping("/delete/{id}")
+    public void deleteProduct(
+        @PathVariable Long id
+    ) {
+        productServiceMvc.deleteById(id);
+    }
 }
