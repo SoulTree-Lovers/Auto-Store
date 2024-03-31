f4dabd6359ba6d1fd8e9881f07b25bfdce07055f
kangseungmin <rkdtmdals999@naver.com>
2024-03-31 16:11:34 +0900

    [feat] Mvc 기반 ProductEntity 속성 추가

--- api/src/main/java/org/example/api/mvc/domain/product/repository/ProductEntityMvc.java
+++ api/src/main/java/org/example/api/mvc/domain/product/repository/ProductEntityMvc.java
@@ -1,14 +1,54 @@
 package org.example.api.mvc.domain.product.repository;
 
 
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.LastModifiedDate;
 
-@Entity
+import java.time.LocalDateTime;
+
+//@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Data
 @Table(name = "product")
 public class ProductEntityMvc {
 
     @Id
+    @Column(nullable = false)
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(name = "admin_id", nullable = false)
+    private Long adminId; // 판매자 아이디
+
+    @Column(length = 100, nullable = false)
+    private String name; // 상품 이름
+
+    @Column(nullable = false)
+    private Long price; // 상품 가격
+
+    @Column(length = 100, nullable = false)
+    private String category; // 상품 카테고리
+
+    @Column(name = "thumbnail_url", length = 200, nullable = false)
+    private String thumbnailUrl; // 상품 썸네일 이미지 주소
+
+    @CreatedDate
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
 
 }
