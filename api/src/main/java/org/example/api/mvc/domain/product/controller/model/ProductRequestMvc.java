7b7b24826f4ad0003e092ea42edcdf97eae71ce0
kangseungmin <rkdtmdals999@naver.com>
2024-03-31 16:12:13 +0900

    [feat] Mvc 기반 ProductRequest DTO 속성 추가

--- api/src/main/java/org/example/api/mvc/domain/product/controller/model/ProductRequestMvc.java
+++ api/src/main/java/org/example/api/mvc/domain/product/controller/model/ProductRequestMvc.java
@@ -6,11 +6,25 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDateTime;
+
 @Data
 @NoArgsConstructor
 //@AllArgsConstructor
-@Builder
 public class ProductRequestMvc {
 
+    private Long adminId; // 판매자 아이디
+
+    private String name; // 상품 이름
+
+    private Long price; // 상품 가격
+
+    private String category; // 상품 카테고리
+
+    private String thumbnailUrl; // 상품 썸네일 이미지 주소
+
+    private LocalDateTime createdAt;
+
+    private LocalDateTime updatedAt;
 
 }
