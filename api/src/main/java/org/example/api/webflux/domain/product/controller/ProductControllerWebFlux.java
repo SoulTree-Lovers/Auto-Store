f964b8cf2b324637ec4aa167ed92d02e571ff1a0
kangseungmin <rkdtmdals999@naver.com>
2024-03-31 16:11:11 +0900

    [feat] Webflux 기반 Product Contoller CRUD 구현

--- api/src/main/java/org/example/api/webflux/domain/product/controller/ProductControllerWebFlux.java
+++ api/src/main/java/org/example/api/webflux/domain/product/controller/ProductControllerWebFlux.java
@@ -1,14 +1,66 @@
 package org.example.api.webflux.domain.product.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import lombok.RequiredArgsConstructor;
+import org.example.api.webflux.domain.product.controller.model.ProductRequestWebFlux;
+import org.example.api.webflux.domain.product.controller.model.ProductResponseWebFlux;
+import org.example.api.webflux.domain.product.service.ProductServiceWebFlux;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 @RestController
-@RequestMapping("/product")
+@RequestMapping("/web-flux/product")
+@RequiredArgsConstructor
 public class ProductControllerWebFlux {
 
     /**
      * TODO: 상품 CRUD 요청 구현
      */
 
+    private final ProductServiceWebFlux productServiceWebFlux;
+
+    // Create
+    @PostMapping("/create")
+    public Mono<ProductResponseWebFlux> createProduct(
+        @RequestBody ProductRequestWebFlux request
+        ) {
+
+        return productServiceWebFlux.create(
+            request.getAdminId(),
+            request.getName(),
+            request.getPrice(),
+            request.getCategory(),
+            request.getThumbnailUrl()
+        ).map(ProductResponseWebFlux::of);
+
+    }
+
+    // Read
+    @GetMapping("/find-all")
+    public Flux<ProductResponseWebFlux> findAllProduct() {
+        return productServiceWebFlux.findAll()
+            .map(ProductResponseWebFlux::of);
+    }
+
+    @GetMapping("/find/{id}")
+    public Mono<ResponseEntity<ProductResponseWebFlux>> findProductById(
+        @PathVariable Long id
+    ) {
+        return productServiceWebFlux.findById(id)
+            .map(it -> ResponseEntity.ok().body(ProductResponseWebFlux.of(it)))
+            .switchIfEmpty(Mono.just(ResponseEntity.notFound().build()));
+    }
+
+    // Update
+
+
+    // Delete
+    @DeleteMapping("/delete/{id}")
+    public Mono<ResponseEntity<?>> deleteProduct(
+        @PathVariable Long id
+    ) {
+        return productServiceWebFlux.deleteById(id)
+            .then(Mono.just(ResponseEntity.noContent().build()));
+    }
 }
