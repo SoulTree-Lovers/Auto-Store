c386a217233133a07d076c2d2992a7293077e3ae
kangseungmin <rkdtmdals999@naver.com>
2024-03-31 16:11:55 +0900

    [feat] Repository 어노테이션 추가

--- api/src/main/java/org/example/api/mvc/domain/product/repository/ProductRepositoryMvc.java
+++ api/src/main/java/org/example/api/mvc/domain/product/repository/ProductRepositoryMvc.java
@@ -1,6 +1,11 @@
 package org.example.api.mvc.domain.product.repository;
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
+@Qualifier(value = "product")
 public interface ProductRepositoryMvc extends JpaRepository<ProductEntityMvc, Long> {
+
 }
