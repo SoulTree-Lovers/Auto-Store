4c7ec5ecbfa2e1fa850c53d98a625b960cd52c42
kangseungmin <rkdtmdals999@naver.com>
2024-04-02 09:49:09 +0900

    [refactor] Table 이름을 product_mvc로 변경

--- api/src/main/java/org/example/api/mvc/domain/product/repository/ProductRepositoryMvc.java
+++ api/src/main/java/org/example/api/mvc/domain/product/repository/ProductRepositoryMvc.java
@@ -1,11 +1,13 @@
 package org.example.api.mvc.domain.product.repository;
 
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.stereotype.Repository;
 
 @Repository
-@Qualifier(value = "product")
+@Qualifier(value = "product_mvc")
 public interface ProductRepositoryMvc extends JpaRepository<ProductEntityMvc, Long> {
 
 }
