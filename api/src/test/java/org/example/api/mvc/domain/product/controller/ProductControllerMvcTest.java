8d80166480e3150ddcdbdb8f2fe4906e3734e199
kangseungmin <rkdtmdals999@naver.com>
2024-04-05 10:54:31 +0900

    [test] 상품 등록 테스트 구현

--- api/src/test/java/org/example/api/mvc/domain/product/controller/ProductControllerMvcTest.java
+++ api/src/test/java/org/example/api/mvc/domain/product/controller/ProductControllerMvcTest.java
@@ -1,13 +1,23 @@
 package org.example.api.mvc.domain.product.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.example.api.mvc.domain.product.controller.model.ProductRequestMvc;
+import org.example.api.mvc.domain.product.controller.model.ProductResponseMvc;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.time.LocalDateTime;
+
 import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -15,7 +25,8 @@ class ProductControllerMvcTest {
     /**
      * TODO: 상품 CRUD 테스트, 상품 여러 개 등록 시 성능 측정 로직 구현
      */
-
+    @Autowired
+    ObjectMapper objectMapper;
 
     @Autowired
     MockMvc mvc;
@@ -25,15 +36,42 @@ class ProductControllerMvcTest {
 
     @Test
     @DisplayName("상품 등록")
-    void createProduct() {
+    void createProduct() throws Exception {
         // given
+        Long adminId = 1L;
+        String name = "보조배터리";
+        Long price = 10000L;
+        String category = "전자기기";
+        String thumbnailUrl = "http://abc.com";
 
 
         // when
+        String body = objectMapper.writeValueAsString(ProductRequestMvc.builder()
+            .adminId(adminId)
+            .name(name)
+            .price(price)
+            .category(category)
+            .thumbnailUrl(thumbnailUrl)
+            .build());
 
 
         // then
+        /*ProductResponseMvc productResponseMvc = ProductResponseMvc.builder()
+            .id(1L)
+            .adminId(adminId)
+            .name(name)
+            .price(price)
+            .category(category)
+            .thumbnailUrl(thumbnailUrl)
+            .createdAt(LocalDateTime.now())
+            .updatedAt(LocalDateTime.now())
+            .build();*/
 
+        mvc.perform(post(BASE_URL + "/create")
+            .content(body)
+            .contentType(MediaType.APPLICATION_JSON))
+            .andExpect(status().isOk());
+//            .andExpect(content().string(objectMapper.writeValueAsString(productResponseMvc)));
 
     }
 
