8f6e37a3d1d4fa7215e3abe0a6c95590142d64f5
kangseungmin <rkdtmdals999@naver.com>
2024-04-14 13:52:16 +0900

    feat: WebFlux 기반 상품 전체 조회 테스트 코드 작성

--- api/src/test/java/org/example/api/webflux/domain/product/controller/ProductControllerWebFluxTest.java
+++ api/src/test/java/org/example/api/webflux/domain/product/controller/ProductControllerWebFluxTest.java
@@ -1,9 +1,13 @@
 package org.example.api.webflux.domain.product.controller;
 
+import org.example.api.mvc.domain.product.repository.ProductRepositoryMvc;
+import org.example.api.mvc.domain.product.service.ProductServiceMvc;
 import org.example.api.webflux.domain.product.controller.model.ProductRequestWebFlux;
 import org.example.api.webflux.domain.product.controller.model.ProductResponseWebFlux;
 import org.example.api.webflux.domain.product.repository.ProductEntityWebFlux;
+import org.example.api.webflux.domain.product.repository.ProductRepositoryWebFlux;
 import org.example.api.webflux.domain.product.service.ProductServiceWebFlux;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
@@ -11,6 +15,7 @@ import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.reactive.server.WebTestClient;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 import java.time.LocalDateTime;
@@ -26,8 +31,20 @@ class ProductControllerWebFluxTest {
     @Autowired
     private WebTestClient webTestClient;
 
+//    @Autowired
+//    ProductServiceWebFlux productServiceWebFlux;
+
     @MockBean
     private ProductServiceWebFlux productServiceWebFlux;
+
+//    @Autowired
+//    ProductRepositoryWebFlux productRepositoryWebFlux;
+
+//    @BeforeEach
+//    void setUp() {
+//        // 각 테스트 실행 전에 데이터베이스를 초기화.
+//        productRepositoryWebFlux.deleteAll();
+//    }
 
     @Test
     void createProduct() {
@@ -38,10 +55,12 @@ class ProductControllerWebFluxTest {
         String category = "전자기기";
         String thumbnailUrl = "http://abc.com";
 
+        // when
         when(productServiceWebFlux.create(adminId, name, price, category, thumbnailUrl)).thenReturn(
             Mono.just(new ProductEntityWebFlux(1L, adminId, name, price, category, thumbnailUrl, LocalDateTime.now(), LocalDateTime.now()))
         );
 
+        // then
         webTestClient.post().uri("/web-flux/product/create")
             .contentType(MediaType.APPLICATION_JSON)
             .bodyValue(new ProductRequestWebFlux(adminId, name, price, category, thumbnailUrl))
@@ -59,6 +78,23 @@ class ProductControllerWebFluxTest {
 
     @Test
     void findAllProduct() {
+        // given
+        // when
+        when(productServiceWebFlux.findAll()).thenReturn(
+            Flux.just(
+                new ProductEntityWebFlux(1L, 1L, "상품1", 10000L, "전자기기", "https://naver.com?213asdabfd", LocalDateTime.now(), LocalDateTime.now()),
+                new ProductEntityWebFlux(2L, 1L, "상품2", 15000L, "생활용품", "https://naver.com?213asdaa", LocalDateTime.now(), LocalDateTime.now()),
+                new ProductEntityWebFlux(3L, 1L, "상품3", 20000L, "주방용품", "https://naver.com?213asdadsagg", LocalDateTime.now(), LocalDateTime.now())
+            )
+        );
+
+        // then
+        webTestClient.get().uri("/web-flux/product/find-all")
+            .exchange()
+            .expectStatus().is2xxSuccessful()
+            .expectBodyList(ProductResponseWebFlux.class)
+            .hasSize(3);
+
     }
 
     @Test
