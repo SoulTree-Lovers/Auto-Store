6b6fbb4257383dc78e17507452f47000e1cf0e58
keib <55739289+kokyeongbin@users.noreply.github.com>
2024-05-15 20:15:23 +0900

    Delete memorydbToJPA directory

--- memorydbToJPA/src/main/java/com/example/memorydb/book/db/repository/BookRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.memorydb.book.db.repository;
-
-import com.example.memorydb.book.db.entity.BookEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Service;
-
-@Service
-public interface BookRepository extends JpaRepository<BookEntity, Long> {
-}
