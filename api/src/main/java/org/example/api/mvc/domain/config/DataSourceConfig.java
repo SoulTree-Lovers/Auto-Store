b5ddffa987d5a08abb6751f7400ae099fca7683f
kangseungmin <rkdtmdals999@naver.com>
2024-05-17 15:09:09 +0900

    refactor: DataSource config 수정

--- api/src/main/java/org/example/api/mvc/domain/config/DataSourceConfig.java
+++ api/src/main/java/org/example/api/mvc/domain/config/DataSourceConfig.java
@@ -15,9 +15,10 @@ public class DataSourceConfig {
     public DataSource dataSource() {
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
         dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
-        dataSource.setUrl("jdbc:mysql://localhost:3320/autostore");
+//        dataSource.setUrl("jdbc:mysql://localhost:3320/autostore");
+        dataSource.setUrl("jdbc:mysql://autostore.cle2yiiugd0z.ap-southeast-2.rds.amazonaws.com:3306/autostoreDB?serverTimezone=Asia/Seoul&characterEncoding=UTF-8");
         dataSource.setUsername("root");
-        dataSource.setPassword("root");
+        dataSource.setPassword("root1234!!");
         return dataSource;
 //        EmbeddedDatabaseBuilder builder = new EmbeddedDatabaseBuilder();
 //        return builder.setType(EmbeddedDatabaseType.HSQL).build();
